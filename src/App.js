54d6b866e20aff6caab17cc94d6614dbea4d427e
Joel Herrero <joelv2@gmail.com>
2023-02-22 12:26:28 +0100

    Scaffolding and initial files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Layout} from "./components/Layout/Layout"
+import Home from "./pages/Home/Home";
+import Detail from "./pages/Detail/Detail";
+import NotFoundPage from "./pages/NotFound/NotFound";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/detail" element={Detail} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Route>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
