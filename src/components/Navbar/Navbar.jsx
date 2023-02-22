1903f086d82a73a51dec693c35d48bc244742a65
ChristianCarbajo <christian_carbajo@hotmail.com>
2023-02-22 18:22:19 +0100

    added navBar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,21 @@
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+import logo from "./Img-Nav/Logo.png";
+import {Scroll} from 'react-scroll';
 
 const Navbar = () => {
   return (
-    <header className="">
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact">Contact</NavLink>
-            </li>
-          </ul>
-        </nav>
+    <header className="navBar">
+      <img src={logo}></img>
+      <div className="navBar-line"></div>
+            <div className="navBar-elements">
+              <h3>Home</h3>
+              <h3>Voices</h3>
+              <h3>Work with us</h3>
+              <h3>Reviews</h3>
+            </div>
+        
       </header>
   )
 }
