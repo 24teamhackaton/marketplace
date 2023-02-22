36317c4399d2789217f8a5d3c30601163e901b0b
Joel Herrero <joelv2@gmail.com>
2023-02-22 14:30:08 +0100

    layout add

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-
+import styles from "./Layout.css";
 
 export const Layout = () => {
   return (
     <>
       <Navbar />
-      <main>
+      <main className={styles.main}>
+        <div>
         <Outlet />
+        </div>
       </main>
       <Footer />
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <header>
+    <header className="">
         <nav>
           <ul>
             <li>
