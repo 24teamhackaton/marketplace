12ac0148f63cd6db95d06ff990149b01b34a0b45
Joel Herrero <joelv2@gmail.com>
2023-02-23 17:38:11 +0100

    navbar fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,51 +14,67 @@ const Navbar = () => {
 
   return (
     <header id="NavBar" className="navBar">
-      <img className="navBar-logo" alt="company logo "src={logo}></img>
+      <img className="navBar-logo" alt="company logo " src={logo}></img>
       <div className="navBar-line"></div>
       <button onClick={ToogleMenu} className="navBar-button">
-              <img src={HamburgerIcon} alt="icon hamburger"></img>
-            </button>
-            <nav className={`navBar-elements ${menu ? 'isActive' : ''}`}>
-              <h3><Link
-                activeClass="active"
-                to=""
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >Home</Link></h3>
-              <h3><Link
-                activeClass="active"
-                to="soundBox"
-                spy={true}
-                smooth={true}
-                offset={-270}
-                duration={500}>Voices</Link></h3>
-              <h3><Link
-              activeClass="active"
-              to="workWithUs"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}>Work with us</Link></h3>
-              <h3><Link
-              activeClass="active"
-              to=""
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}>Reviews</Link></h3>
-            
-            </nav>
-            
-            
-            
-            
-            
-        
-      </header>
-  )
-}
+        <img src={HamburgerIcon} alt="icon hamburger"></img>
+      </button>
+      <nav className={`navBar-elements ${menu ? "isActive" : ""}`}>
+        <h3>
+          <Link
+            className="navBar-link"
+            activeClass="active"
+            to=""
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Home
+          </Link>
+        </h3>
+        <h3>
+          <Link
+            className="navBar-link"
+            activeClass="active"
+            to="soundBox"
+            spy={true}
+            smooth={true}
+            offset={-270}
+            duration={500}
+          >
+            Voices
+          </Link>
+        </h3>
+        <h3>
+          <Link
+            className="navBar-link"
+            activeClass="active"
+            to="workWithUs"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Work with us
+          </Link>
+        </h3>
+        <h3>
+          <Link
+            className="navBar-link"
+            activeClass="active"
+            to=""
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Reviews
+          </Link>
+        </h3>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
